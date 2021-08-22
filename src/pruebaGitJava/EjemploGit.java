91cf5b44452b52a6ed07b23a7532bb62717ac9be
serrudjc <serrudo.jc@gmail.com>
2021-08-21 21:48:47 -0300

    Comienzo de proyecto

---
+++ src/pruebaGitJava/EjemploGit.java
@@ -0,0 +1,12 @@
+package pruebaGitJava;
+
+public class EjemploGit
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Hola Git");
+	}
+
+}
